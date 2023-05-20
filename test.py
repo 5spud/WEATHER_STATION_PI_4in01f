2df9ddc057c968aee02ce3cf3fe1ca2dd87da38c
5spud <91005280+5spud@users.noreply.github.com>
2023-05-21 00:15:55 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,5 +10,11 @@ time.sleep(2)
 epd.init()
 # print("Exiting !")
 # doesn´t exit: epd.Dev_exit()
+   # FRAME
+    display.draw_black.rectangle((5, 5, 795, 475), fill=255, outline=0, width=2)  # INNER FRAME
+    display.draw_black.line((540, 5, 540, 350), fill=0, width=1)  # VERTICAL SEPARATION
+    display.draw_black.line((350, 5, 350, 350), fill=0, width=1)  # VERTICAL SEPARATION slim
+    display.draw_black.line((5, 350, 795, 350), fill=0, width=1)  # HORIZONTAL SEPARATION
+
 print("Bye")
 exit()
