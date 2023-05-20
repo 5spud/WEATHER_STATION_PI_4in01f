2d5c4a602acb634949511ec43120a7c1f7aabe29
5spud <91005280+5spud@users.noreply.github.com>
2023-05-21 00:26:45 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import epd4in01f
 import time
+from weather import *
+from news import *
+from display import *
+import json
 print("Starting shut down")
 epd = epd4in01f.EPD()
 epd.init()
@@ -11,6 +15,7 @@ epd.init()
 # print("Exiting !")
 # doesn´t exit: epd.Dev_exit()
    # FRAME
+    display = Display()
     display.draw_black.rectangle((5, 5, 795, 475), fill=255, outline=0, width=2)  # INNER FRAME
     display.draw_black.line((540, 5, 540, 350), fill=0, width=1)  # VERTICAL SEPARATION
     display.draw_black.line((350, 5, 350, 350), fill=0, width=1)  # VERTICAL SEPARATION slim
